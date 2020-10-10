479f3d31798513580ce1b66737eb36c278b54c61
Alessandro <alessandroadm@live.com>
2020-10-10 17:45:00 -0300

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
-    jest:true
+    jest: true
   },
   extends: [
     'standard'
@@ -16,12 +16,12 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
-    "quotes": ["error", "single"],
-    "semi": ["error", "always"],
-      "@typescript-eslint/strict-boolean-expressions":"off",
-      "@typescript-eslint/consistent-type-definitions":"off",
-      "@typescript-eslint/comma-spacing":"off",
-      "@typescript-eslint/method-signature-style":"off",
-      "@typescript-eslint/semi":"off"
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/comma-spacing': 'off',
+    '@typescript-eslint/method-signature-style': 'off',
+    '@typescript-eslint/semi': 'off'
   }
-}
+};
